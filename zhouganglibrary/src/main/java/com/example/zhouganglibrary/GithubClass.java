12f9e90f8af9dd7c0fdc00bea35df2c6073d013f
zhougang <609568681@qq.com>
2020-06-28 12:28:18 +0800

    修改吐司

--- zhouganglibrary/src/main/java/com/example/zhouganglibrary/GithubClass.java
+++ zhouganglibrary/src/main/java/com/example/zhouganglibrary/GithubClass.java
@@ -5,6 +5,6 @@ import android.widget.Toast;
 
 public class GithubClass {
     public static void showToast(Context context) {
-        Toast.makeText(context,"ceshi1111",Toast.LENGTH_LONG).show();
+        Toast.makeText(context,"ceshi111122222",Toast.LENGTH_LONG).show();
     }
 }
