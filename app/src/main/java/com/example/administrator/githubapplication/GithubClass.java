a15384eed3d55b624c9cea112a9c2231c37a5e84
zhougang <609568681@qq.com>
2020-07-02 16:08:47 +0800

    修改路径到主项目

--- app/src/main/java/com/example/administrator/githubapplication/GithubClass.java
+++ app/src/main/java/com/example/administrator/githubapplication/GithubClass.java
@@ -1,4 +1,4 @@
-package com.example.zhouganglibrary;
+package com.example.administrator.githubapplication;
 
 import android.content.Context;
 import android.widget.Toast;
--- app/src/main/java/com/example/administrator/githubapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.administrator.githubapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.zhouganglibrary.GithubClass;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GithubClass.showToast(this);
     }
 }
